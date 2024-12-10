f035ab65c9e08a7ebb1c0d5c86a23f6bfea16d7a
Linli Shi <cnshilinli@gmail.com>
2024-12-10 16:08:31 +0000

    test

---
+++ cliptest.py
@@ -0,0 +1,19 @@
+import torch
+import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/16", device=device)
+
+image = preprocess(Image.open("./tt.jpg")).unsqueeze(0).to(device)
+text = clip.tokenize(["a turtle", "a dog", "a Squirtle"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    print(image_features.shape, text_features.shape)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
